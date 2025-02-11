064b1ce5b824a2de4a700bc5b2adc60c988e3abc
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2025-02-12 01:05:38 +0530

    production ready

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiUrl="https://68bfba3a-628b-4a7e-8eb7-36b81c44d634-dev.e1-us-east-azure.choreoapis.dev/notesmakingapp/server/v1/";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL?import.meta.env.VITE_API_URL:apiUrl,
+  baseURL: apiUrl,
 });
 
 api.interceptors.request.use(
