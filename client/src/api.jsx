3f17d3a2c8fc936caf24cef696fff290ec41d7f4
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2025-02-12 01:09:42 +0530

    deploy frontend on vercel

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const apiUrl="https://68bfba3a-628b-4a7e-8eb7-36b81c44d634-dev.e1-us-east-azure.choreoapis.dev/notesmakingapp/server/v1/";
-
 const api = axios.create({
-  baseURL: apiUrl,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
